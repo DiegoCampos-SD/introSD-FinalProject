c4f2df1fed913a6f748a56e77954f1fc4b682141
DieCamDu <dcamposd@ucenfotec.ac.cr>
2024-09-29 15:30:25 -0400

    Forth commit // Creation of welcome window, with start, info and exit buttons and funtionality of those buttons. Also, creation of exit button in main window, change on conditional paremeter of password lenght too.

--- guardianLock.py
+++ guardianLock.py
@@ -11,45 +11,77 @@ choose a secure password every time that the user requires it.
 '''
 
 import tkinter as tk
-from tkinter import messagebox, simpledialog, PhotoImage
+from tkinter import messagebox, simpledialog, PhotoImage, Frame,Toplevel
 import random
 import string
 
 # Main Application Class
-class GuardianLockApp:
+class guardianLockApp:
     def __init__(self, root):
         self.root = root
+        self.root.attributes('-toolwindow', True)
         self.root.title("Password Maker Helper")
-        self.root.geometry("500x400")
+        self.root.geometry("400x200+400+300")
         self.root.resizable(0,0)
         
         #Background-image
-        self.imagePath = tk.PhotoImage(file="bg2.png")
-        self.img_copy= self.imagePath.copy()
+        self.imagePath = tk.PhotoImage(file="bg1.png")
         self.background = tk.Label(self.root, image= self.imagePath)
-        self.background.place(relx=0.4, rely=0.35, anchor="center")
+        self.background.place(relx=0.2, rely=0.75, anchor="center")
         
         # Header
-        self.header_label = tk.Label(root, text="Password Maker Helper", font=("Arial", 16))
+        self.header_label = tk.Label(root, text="Welcome to Password Maker Helper", font=("Arial", 16), background="#d5eafd")
         self.header_label.pack(pady=10)
+        
+        # Button frame to contain buttons
+        self.buttonFrame = tk.Frame(root, background="#caf0f8")
+        self.buttonFrame.pack(pady=20)
+
+        # Buttons creation
+        # Start app button
+        self.generateButton = tk.Button(self.buttonFrame, text="Start", height=1, width=4, font=("Arial", 12), fg='white', background="#588157", command=self.createPasswordGeneration) #Button to start the app
+        self.generateButton.pack(side="left", padx=5)
+        
+        # Information button
+        self.generateButton = tk.Button(self.buttonFrame, text="Info", height=1, width=4, font=("Arial", 12), fg="white", background="#669bbc", command=self.showInfo) #Button to show app information
+        self.generateButton.pack(side="left", padx=5)
+        
+        # Exit button
+        self.generateButton = tk.Button(self.buttonFrame, text="Exit", height=1, width=4, font=("Arial", 12), fg="white", background="#780000", command=self.root.destroy) #Button to exit the app
+        self.generateButton.pack(side="left", padx=5)
 
         # Password Generation
-        self.createPasswordGeneration()
+#        self.createPasswordGeneration()
 
-    def _resize_image(self,event):
-
-        new_width = event.width
-        new_height = event.height
-
-        self.image = self.img_copy.resize((new_width, new_height))
-
-        self.background_image = ImageTk.PhotoImage(self.image)
-        self.background.configure(image =  self.background_image)
+    def showInfo(self):
+        messagebox.showinfo("App information", "A password maker app is a tool designed " +
+                            "to generate strong, unique passwords for users. This app " + 
+                            "offers features like customizable length, inclusion of special " +
+                            "characters, uppercase letters, lowercase letters and numbers. Also " +
+                            "this app includes a copy to clipboard option, so the user don't have " +
+                            "to memorize the generated app. This helps users enhance their online security " + 
+                            "and simplifies the process of managing multiple accounts.")
+        
+    
     
     def createPasswordGeneration(self):
+        
+        # Creats new window
+        self.newWindow = tk.Toplevel()
+        self.newWindow.attributes('-toolwindow', True)
+        self.newWindow.overrideredirect()
+        self.newWindow.geometry("500x350+400+300")
+        self.newWindow.resizable(0,0)
+        self.root.iconify()
+        
+        # Background image
+        self.imagePath = tk.PhotoImage(file="bg2.png")
+        self.background = tk.Label(self.newWindow, image= self.imagePath)
+        self.background.place(relx=0.25, rely=0.25, anchor="center")
+        
         # Password Length
-        tk.Label(self.root, text="Password Length:").pack(pady=5)
-        self.lengthEntry = tk.Entry(self.root)
+        tk.Label(self.newWindow, text="Password Length:", font=("Arial", 16), background="#e2d0fd").pack(pady=5)
+        self.lengthEntry = tk.Entry(self.newWindow)
         self.lengthEntry.pack(pady=5)
 
         # Complexity Checkboxes
@@ -58,22 +90,26 @@ class GuardianLockApp:
         self.includeNumbers = tk.BooleanVar()
         self.includeSpecial = tk.BooleanVar()
 
-        tk.Checkbutton(self.root, text="Include Uppercase Letters", variable=self.includeUppercase).pack()
-        tk.Checkbutton(self.root, text="Include Lowercase Letters", variable=self.includeLowercase).pack()
-        tk.Checkbutton(self.root, text="Include Numbers", variable=self.includeNumbers).pack()
-        tk.Checkbutton(self.root, text="Include Special Characters", variable=self.includeSpecial).pack()
+        tk.Checkbutton(self.newWindow, text="Include Uppercase Letters", font=("Arial", 12), background="#e2d0fd", variable=self.includeUppercase).pack()
+        tk.Checkbutton(self.newWindow, text="Include Lowercase Letters", font=("Arial", 12),background="#e2d0fd", variable=self.includeLowercase).pack()
+        tk.Checkbutton(self.newWindow, text="Include Numbers", font=("Arial", 12), background="#e2d0fd", variable=self.includeNumbers).pack()
+        tk.Checkbutton(self.newWindow, text="Include Special Characters", font=("Arial", 12), background="#e2d0fd", variable=self.includeSpecial).pack()
 
         # Generate Button
-        self.generateButton = tk.Button(self.root, text="Generate Password", command=self.generatePassword)
+        self.generateButton = tk.Button(self.newWindow, text="Generate Password", font=("Arial", 12), background="#e2d0fd", command=self.generatePassword)
         self.generateButton.pack(pady=10)
 
         # Display Generated Password
-        self.passwordDisplay = tk.Entry(self.root, width=50)
+        self.passwordDisplay = tk.Entry(self.newWindow, width=50)
         self.passwordDisplay.pack(pady=5)
 
         # Copy Button
-        self.copyButton = tk.Button(self.root, text="Copy to Clipboard", command=self.copyToClipboard)
+        self.copyButton = tk.Button(self.newWindow, text="Copy to Clipboard", font=("Arial", 12), background="#e2d0fd", command=self.copyToClipboard)
         self.copyButton.pack(pady=5)
+        
+        # Exit Button
+        self.generateButton = tk.Button(self.newWindow, text="Exit", height=1, width=4, font=("Arial", 12), fg="white", background="dark red", command=self.root.destroy) #Button to exit the app
+        self.generateButton.pack(side="right", padx=10, pady=10)
 
     def generatePassword(self):
         length = self.getPasswordLength()
@@ -101,11 +137,11 @@ class GuardianLockApp:
     def getPasswordLength(self):
         try:
             length = int(self.lengthEntry.get())
-            if length <= 0:
+            if length < 8 :
                 raise ValueError
             return length
         except ValueError:
-            messagebox.showerror("Error", "Please enter a valid positive integer for password length.")
+            messagebox.showerror("Error", "Please enter a valid positive integer grader than 7 for password length.")
             return None
 
     def copyToClipboard(self):
@@ -113,12 +149,12 @@ class GuardianLockApp:
         if password == "":
             messagebox.showerror("Error", "You must create a password to use this feature.")
             return
-        self.root.clipboard_clear()
-        self.root.clipboard_append(password)
+        self.newWindow.clipboard_clear()
+        self.newWindow.clipboard_append(password)
         messagebox.showinfo("Info", "Password copied to clipboard.")
 
 # Run Application
 if __name__ == "__main__":
     root = tk.Tk()
-    app = GuardianLockApp(root)
+    app = guardianLockApp(root)
     root.mainloop()
